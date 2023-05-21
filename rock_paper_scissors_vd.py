f0d6d2ee356517a2b9dbc7e4588091a0f8e76a66
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2023-05-21 12:22:41 +0300

    Update rock_paper_scissors_vd.py
    added options

--- rock_paper_scissors_vd.py
+++ rock_paper_scissors_vd.py
@@ -4,39 +4,51 @@ rock = 'Rock'
 paper = 'Paper'
 scissors = 'Scissors'
 computer_move = ""
-player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
-if player_move == 'r':
-    player_move = rock
-elif player_move == 'p':
-    player_move = paper
-elif player_move == 's':
-    player_move == scissors
-else:
-    raise SystemExit(f'Invalid Input. {player_name}, please try again...')
+player_move = ''
+while True:
+    player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
+    if player_move == 'r':
+        player_move = rock
+    elif player_move == 'p':
+        player_move = paper
+    elif player_move == 's':
+        player_move == scissors
+    else:
+        raise SystemExit(f'Invalid Input. {player_name}, please try again...')
 
-computer_random_number = random.randint(1, 3)
 
-if computer_random_number == 1:
-    computer_move = rock
 
-elif computer_random_number ==2:
-    computer_move = paper
-else:
-    computer_move = scissors
+    computer_random_number = random.randint(1, 3)
 
-print(f"The computer chose {computer_move}.")
+    if computer_random_number == 1:
+        computer_move = rock
 
-if (player_move == rock and computer_move == scissors) or \
-        (player_move == paper and computer_move == rock) or \
-        (player_move == scissors and computer_move == paper):
-    print(f'You win, {player_name} ! Congratulations !')
-elif (player_move == rock and computer_move == rock) or \
-        (player_move == paper and computer_move == paper) or \
-        (player_move == scissors and computer_move == scissors):
-    print('We have draw !')
+    elif computer_random_number ==2:
+        computer_move = paper
+    else:
+        computer_move = scissors
 
-else:
-    print(f'You lose, {player_name}. Better luck next time!!!')
+    print(f"The computer chose {computer_move}.")
+
+    if (player_move == rock and computer_move == scissors) or \
+            (player_move == paper and computer_move == rock) or \
+            (player_move == scissors and computer_move == paper):
+        print(f'You win, {player_name} ! Congratulations !')
+    elif (player_move == rock and computer_move == rock) or \
+            (player_move == paper and computer_move == paper) or \
+            (player_move == scissors and computer_move == scissors):
+        print('We have draw !')
+
+    else:
+        print(f'You lose, {player_name}. Better luck next time!!!')
+
+    player_move = input("Type [y] to Play Again or [n] to quit: ")
+    if player_move == 'y':
+        continue
+    elif player_move =='n':
+        break
+
+
     
 
 
